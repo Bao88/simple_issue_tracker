3437db276b37a5d326451878c0b83f8b51d8367b
hoangbn <hoangbn@uio.no>
2021-06-14 16:45:53 +0200

    - Add path. - Remove not necessary codes for now. - Configure the project to proxy requests from front end to backend.

--- server/simple_issue_tracker.js
+++ server/simple_issue_tracker.js
@@ -5,15 +5,13 @@
 const express = require("express");
 const app = express();
 const apiRoutes = require("./routing/api");
-const path = require('path')
+const path = require("path");
 
 // Send the whole build folder to the user or anyone connected to the server
-app.use(express.static(path.join(__dirname, "../client/www")));
-app.use(express.static(path.join(__dirname, "../server/public")));
-app.use(express.json({ limit: "25mb", extended: true }));
-app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.static(path.join(__dirname, "../dist")));
 
-app.use("/api", apiRoutes);
+// API Routes
+app.use('/api', apiRoutes)
 
 // Create server
 const http = require("http");
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const hotHost = "localhost";
+const hotPort = 8080;
+const host = "http://localhost";
+const port = 8000;
+
+module.exports = {
+  publicPath: "",
+  devServer: {
+    index: "index.html",
+    host: hotHost,
+    port: hotPort,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    proxy: {
+      "/api/issues": {
+        target: `${host}:${port}`,
+        changeOrigin: true,
+      },
+    },
+  },
+};
