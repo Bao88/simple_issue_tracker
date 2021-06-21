adbc1fc8a412288c7a34ae88e18272ff4bf0fac9
hoangbn <hoangbn@uio.no>
2021-06-21 16:26:17 +0200

    - commit issues.js

--- server/routing/api/issues.js
+++ server/routing/api/issues.js
@@ -1,66 +1,53 @@
 const router = require("express").Router();
-
-/* We use a dictionary to act as our database in this simple project.
-   We also initialize it with some data.
-*/
-
-let id = 0; //
-class Issue {
-  constructor(issue) {
-    this.id = id++;
-    this.title = issue?.title || "";
-    this.description = issue?.description || "";
-    this.state = issue?.state || "open";
-  }
-}
-
-const issues = new Map();
-
-// Create test data
-const issue1 = new Issue({
-  title: "Create Simple Issue Tracker",
-  description: "Implement viewer and interactions",
-  state: "open",
-});
-
-const issue2 = new Issue({
-  title: "Create Server",
-  description: "Implement the routes",
-  state: "pending",
-});
-
-// Add test data
-issues.set(issue1.id, issue1);
-issues.set(issue2.id, issue2);
+const Issue = require("../../models/Issue");
 
 /* Routes */
 // Get all issues
 router.get("/issues", (request, response) => {
-  const issuesJSON = Object.fromEntries(issues); // Convert Map to JSON
-  response.send(issuesJSON);
+  Issue.find({}, (error, issues) => {
+    if (error) respond.status(400).send(error);
+    else respond.send(issues);
+  });
 });
 
 // Post issue
 router.post("/issue", (request, response) => {
   // create an empty issue
-  const issue = new Issue();
-  issues.set(issue.id, issue);
-  /*
-    Test if body is a valid issue
-    Newly created issues should always have state as 'open" ?
-    if valid create an Issue with new Issue (request.body) and use "set"
-    else send back error(invalid data 422 or 400).
-  */
-  response.send(issue);
+  Issue.create({}, (error, issue) => {
+    if (error) response.status(400).send(error);
+    else response.send(issue);
+  });
 });
 
 // Put/Update an issue
 router.put("/issue", (request, response) => {
+  const canChangeState = (originalState, newState) => {
+    //Edge case if originalState == newState, it means we dont need to update state.
+    if (originalState == newState) return true;
+
+    return (
+      originalState == "open" ||
+      (originalState == "pending" && newState == "closed")
+    );
+  };
+
   const body = request.body;
-
-  // Update the issue
-  issues.set(body.id, body);
-
+  Issue.find({ _id: body.id }, (error, issue) => {
+    if (error) response.status(400).send(error);
+    else {
+      // Test if the state can be changed
+      if (canChangeState(issue.state, body.state)) {
+        // Update the issue
+        Object.assign(issue, body);
+        issue.save((saveError, savedIssue) => {
+          if (saveError) response.status(400).send(saveError);
+          else response.send(savedIssue);
+        });
+      } else {
+        response.status(422).send(new Error("State can't be changed"));
+      }
+    }
+  });
   /*
   We can also test on the backend
     Test if body is a valid issue
